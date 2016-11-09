4276a2a61d05fd974691a2f402e48849ebb0b476
Vincent Chen <applekey@gmail.com>
2016-11-08 21:41:33 -0800

    save

--- main.cu
+++ main.cu
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #define SIGNAL_LENGTH 256
 
+//signal
+double * host_signal_array = 0;
+double * device_signal_array = 0;
+
+//output
+double * device_output_array = 0;
+
+//low filters
+double * host_low_filter_array = 0;
+double * device_low_filter_array = 0;
+
+//high filters
+double * host_high_filter_array = 0;
+double * device_high_filter_array = 0;
+
+
 __device__ void convolveWavelet(double * filter, int filterLength, 
-                                double * inputSignal, int inputIndex,
-                                double * output, int outputIndex) {
+                                double * inputSignal, int signalLength,
+                                double * output, int outputOffset) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int inputIndex = index * 2 + (filterLength - 1); 
+
+    double sum = 0.0;
+
+    for(int i = 0; i < filterLength; i++) {
+        sum += filter[i] * inputSignal[inputIndex];
+    }
+    output[index + outputOffset] = sum; 
 }
 
-__global__ void kernel(double *array)
-{
-  int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-  array[index] ++;
-}
 
 /*void init(double * signal, int signalLength, double * filter, int filterLength) {*/
 double * initSignal() {
     int signalLenght = SIGNAL_LENGTH;
     int num_bytes = signalLenght * sizeof(double);
-
-    double * host_signal_array = 0;
-    double * device_signal_array = 0;
 
     host_signal_array = (double*)malloc(num_bytes);
     cudaMalloc((void**)&device_signal_array, num_bytes);
@@ -32,7 +48,6 @@ double * initOutput() {
     int outputLenght = SIGNAL_LENGTH;
     int num_bytes = outputLenght * sizeof(double);
 
-    double * device_output_array = 0;
     cudaMalloc((void**)&device_output_array, num_bytes);
     return device_output_array;
 }
@@ -40,9 +55,6 @@ double * initOutput() {
 double * initLowFilter() {
     int lowFilterLenght = 9;
     int num_bytes = lowFilterLenght * sizeof(double);
-
-    double * host_low_filter_array = 0;
-    double * device_low_filter_array = 0;
 
     host_low_filter_array = (double*)malloc(num_bytes);
     cudaMalloc((void**)&device_low_filter_array, num_bytes);
@@ -55,9 +67,6 @@ double * initHighFilter() {
     int highFilterLenght = 9;
     int num_bytes = highFilterLenght * sizeof(double);
 
-    double * host_high_filter_array = 0;
-    double * device_high_filter_array = 0;
-
     host_high_filter_array = (double*)malloc(num_bytes);
     cudaMalloc((void**)&device_high_filter_array, num_bytes);
 
@@ -69,53 +78,8 @@ void init() {
     initLowFilter();
     initHighFilter();
     initSignal();
+    initOutput();
 }
-
-
-
-/*
-void init() {
-    int signalLength = SIGNAL_LENGTH;
-
-    int num_bytes = signalLength * sizeof(double);
-    // copy the signal into the device
-    double *device_array = 0;
-    double *host_array = 0;
-
-    // malloc a host signal array
-    host_array = (double*)malloc(num_bytes);
-    //intilize host array with signal
-
-    for(int i = 0; i < signalLength; i++) {
-        host_array[i] = 7.0;
-    }
-
-    // cudaMalloc a device array
-    cudaMalloc((void**)&device_array, num_bytes);
-
-    int block_size = 128;
-    int grid_size = signalLength / block_size;
-
-    //copy input signal to device
-    cudaMemcpy(device_array, host_array, num_bytes, cudaMemcpyHostToDevice);
-
-    kernel<<<grid_size,block_size>>>(device_array);
-
-    //download and inspect the result on the host:
-    cudaMemcpy(host_array, device_array, num_bytes, cudaMemcpyDeviceToHost);
-
-    // print out the result element by element
-    for(int i=0; i < signalLength; ++i) {
-        printf("%f ", host_array[i]);
-    }
-
-    // deallocate memory
-    free(host_array);
-    cudaFree(device_array);
-    
-}
-
-*/
 
 int main(int argc, const char * argv[]) {
     //generate constant signal that is power of 2, 64
